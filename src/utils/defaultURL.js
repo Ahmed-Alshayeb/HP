97147bb3e3a69971781a75b9b0025782da7d4b0d
Ahmed-Alshayeb <ahmdalshayb77@gmail.com>
2024-11-12 00:29:23 +0200

    v1

--- src/utils/defaultURL.js
+++ src/utils/defaultURL.js
@@ -1,1 +1,2 @@
-export const defaultURL = "https://res.cloudinary.com/dpq8atnxn/image/upload/v1730498228/waghed_d4pxmc.jpg";
+export const defaultURL =
+  "https://res.cloudinary.com/dpq8atnxn/image/upload/v1731364036/WhatsApp_Image_2024-08-11_at_19.29.20_94875515_eu9n0f.jpg";

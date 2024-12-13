bab707e0bf85e81e7adcd04c82500827666a088c
Ahmed-Alshayeb <ahmdalshayb77@gmail.com>
2024-12-13 17:32:35 +0200

    v1

--- src/utils/AppError.js
+++ src/utils/AppError.js
@@ -4,7 +4,7 @@ class AppError extends Error {
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith(4) ? "fail" : "error";
     this.isOperational = true;
-    this.stack = err.stack;
+
   }
 }
 export default AppError;

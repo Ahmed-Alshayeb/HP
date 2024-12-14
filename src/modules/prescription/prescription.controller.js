8b3744f88e6521ab013e79bd9c297dabcc36e9eb
Ahmed-Alshayeb <ahmdalshayb77@gmail.com>
2024-12-14 13:47:32 +0200

    v1

--- src/modules/prescription/prescription.controller.js
+++ src/modules/prescription/prescription.controller.js
@@ -3,11 +3,17 @@ import patientModel from "../../../DB/models/patient.model.js";
 import prescriptionModel from "../../../DB/models/prescription.model.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 
+// @desc    get all prescription
+// @route   GET /api/v1/prescription/
+// @access  Private
 export const getPrescriptions = asyncHandler(async (req, res) => {
   const prescriptions = await prescriptionModel.findAll();
   res.status(200).json({ msg: "success", count: prescriptions.length, prescriptions });
 });
 
+// @desc    get single prescription
+// @route   GET /api/v1/prescription/:id
+// @access  Private
 export const getPrescription = asyncHandler(async (req, res) => {
   const prescriptions = await prescriptionModel.findOne({ where: { id: req.params.id } });
   res.status(200).json({ msg: "success", count: prescriptions.length, prescriptions });
@@ -16,7 +22,7 @@ export const getPrescription = asyncHandler(async (req, res) => {
 // @desc    create prescription
 // @route   POST /api/v1/prescription/create
 // @access  Private
-export const createPrescription = async (req, res, next) => {
+export const createPrescription = asyncHandler(async (req, res, next) => {
   const { patient, description, medicines } = req.body;
   const doctor = req.employee.id;
 
@@ -36,12 +42,12 @@ export const createPrescription = async (req, res, next) => {
   });
 
   res.status(201).json({ msg: "success", prescription });
-};
+});
 
 // @desc    get patient prescription
 // @route   POST /api/v1/prescription/get/patient/:id
 // @access  Private
-export const getPatientPrescription = async (req, res, next) => {
+export const getPatientPrescription = asyncHandler(async (req, res, next) => {
   const prescription = await prescriptionModel.findAll({ where: { patient: req.params.id } });
   res.status(201).json({ msg: "success", count: prescription.length, prescription });
-};
+});

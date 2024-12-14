afc62c002ed2f7133141d5e2820d0b974ee807ec
Ahmed-Alshayeb <ahmdalshayb77@gmail.com>
2024-12-14 13:54:14 +0200

    v1

--- src/modules/prescription/prescription.routes.js
+++ src/modules/prescription/prescription.routes.js
@@ -8,6 +8,6 @@ const prescriptionRouter = Router();
 prescriptionRouter.get("/", PC.getPrescriptions);
 prescriptionRouter.get("/:id", PC.getPrescription);
 prescriptionRouter.post("/create", auth(systemRoles.Doctor), PC.createPrescription);
-prescriptionRouter.post("/get/patient/:id", auth(Object.values(systemRoles)), PC.getPatientPrescription);
+prescriptionRouter.get("/get/patient/:id", auth(Object.values(systemRoles)), PC.getPatientPrescription);
 
 export default prescriptionRouter;
